d96501900c2d281870e84c530b358e44b56ff9c9
mabyoungg <mabyoungg@gmail.com>
2023-11-14 09:38:53 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/likelionspringboot/LikelionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelionspringboot/LikelionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
