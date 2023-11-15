ae3ead0fe7e93d7c68ff9d9f06a72b36d6f95501
mabyoungg <mabyoungg@gmail.com>
2023-11-15 23:55:33 +0900

    feat: Request, Response 테스트

--- src/main/java/com/example/likelionspringboot/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/example/likelionspringboot/domain/article/article/controller/ArticleController.java
@@ -3,7 +3,11 @@ package com.example.likelionspringboot.domain.article.article.controller;
 import com.example.likelionspringboot.domain.article.article.entity.Article;
 import com.example.likelionspringboot.domain.article.article.service.ArticleService;
 import com.example.likelionspringboot.global.resultData.ResultData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +40,30 @@ public class ArticleController {
         );
 
         return result;
+    }
+
+    @PostMapping("/article/write2")
+    @SneakyThrows
+    void write2(
+            HttpServletRequest req,
+            HttpServletResponse resp
+    ) {
+        String title = req.getParameter("title");
+        String body = req.getParameter("body");
+
+        Article article = articleService.write(title, body);
+
+        ResultData<Article> resultData = new ResultData<>(
+                "S-1",
+                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
+                article
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().println(objectMapper.writeValueAsString(resultData));
     }
 
     @GetMapping("/article/getLastArticle")
