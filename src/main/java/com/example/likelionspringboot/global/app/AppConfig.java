55aa49685fb60d55326eff64d82cb4feb2c26092
mabyoungg <mabyoungg@gmail.com>
2023-11-16 11:35:47 +0900

    feat: test data 생성

--- src/main/java/com/example/likelionspringboot/global/app/AppConfig.java
+++ src/main/java/com/example/likelionspringboot/global/app/AppConfig.java
@@ -1,16 +1,11 @@
 package com.example.likelionspringboot.global.app;
 
-import com.example.likelionspringboot.domain.article.article.entity.Article;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    List<Article> articles() {
-        return new ArrayList<>();
-    }
+//    @Bean
+//    List<Article> articles() {
+//        return new ArrayList<>();
+//    }
 }
