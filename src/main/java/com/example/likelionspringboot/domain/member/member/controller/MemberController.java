1e8c11913af34f93d43cdb2f313017a94a90f5da
mabyoungg <mabyoungg@gmail.com>
2023-11-17 16:50:31 +0900

    feat: member save 구현

---
+++ src/main/java/com/example/likelionspringboot/domain/member/member/controller/MemberController.java
@@ -0,0 +1,31 @@
+package com.example.likelionspringboot.domain.member.member.controller;
+
+import com.example.likelionspringboot.domain.member.member.service.MemberService;
+import com.example.likelionspringboot.global.rq.Rq;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Validated
+public class MemberController {
+    private final MemberService memberService;
+    private final Rq rq;
+
+    @GetMapping("/member/join")
+    String showJoin() {
+        return "member/member/join";
+    }
+
+    @PostMapping("/member/join")
+    String join(@NotBlank String username,
+                @NotBlank String password) {
+        memberService.join(username, password);
+
+        return rq.redirect("/member/login", "회원가입이 완료되었습니다.");
+    }
+}
---
+++ src/main/java/com/example/likelionspringboot/domain/member/member/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.likelionspringboot.domain.member.member.service;
+
+import com.example.likelionspringboot.domain.member.member.entity.Member;
+import com.example.likelionspringboot.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member join(String username, String password) {
+        Member member = new Member(username, password);
+
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findById(long id) {
+        return memberRepository.findById(id);
+    }
+
+    public void delete(long id) {
+        memberRepository.delete(id);
+    }
+
+    public void modify(long id, String username, String password) {
+        Member member = findById(id).get();
+        member.setUsername(username);
+        member.setPassword(password);
+    }
+}
