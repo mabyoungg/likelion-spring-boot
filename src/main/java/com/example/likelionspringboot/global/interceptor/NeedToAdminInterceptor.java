4dd49114006e25389062763cb5c700c5ec857bf4
mabyoungg <mabyoungg@gmail.com>
2023-11-22 18:34:25 +0900

    fix: admin check method 변경

--- src/main/java/com/example/likelionspringboot/global/interceptor/NeedToAdminInterceptor.java
+++ src/main/java/com/example/likelionspringboot/global/interceptor/NeedToAdminInterceptor.java
@@ -7,19 +7,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.util.List;
-
 @Component
 @RequiredArgsConstructor
 public class NeedToAdminInterceptor implements HandlerInterceptor {
     private final Rq rq;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        List<String> authorities = rq.getSessionAttribute("authorities");
-
-        if (!authorities.contains("ROLE_ADMIN")) {
+        if (!rq.isAdmin()) {
             throw new RuntimeException("관리자만 이용할 수 있는 페이지 입니다.");
         }
+
         return true;
     }
 }
