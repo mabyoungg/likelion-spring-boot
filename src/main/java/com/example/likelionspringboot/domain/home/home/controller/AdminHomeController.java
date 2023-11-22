4690a913cb51475d7f84df0010ae91d71e3607ad
mabyoungg <mabyoungg@gmail.com>
2023-11-22 20:57:49 +0900

    refactor: authorizeHttpRequests 적용

--- src/main/java/com/example/likelionspringboot/domain/home/home/controller/AdminHomeController.java
+++ src/main/java/com/example/likelionspringboot/domain/home/home/controller/AdminHomeController.java
@@ -1,23 +1,15 @@
 package com.example.likelionspringboot.domain.home.home.controller;
 
-import com.example.likelionspringboot.global.rq.Rq;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class AdminHomeController {
-    private final Rq rq;
-
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String showMain() {
         return "home/home/admin/main";
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/home/about")
     public String showAbout() {
         return "home/home/admin/about";
