d2a8ab4aa42720ebdfa4ce2b8b5df8ee769dbb35
mabyoungg <mabyoungg@gmail.com>
2023-11-25 00:43:29 +0900

    test: 로그인 페이지 test code

---
+++ src/test/java/com/example/likelionspringboot/domain/member/member/controller/MemberControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.likelionspringboot.domain.member.member.controller;
+
+import com.example.likelionspringboot.domain.member.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class MemberControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private MemberService memberService;
+
+    // GET /member/login
+    @Test
+    @DisplayName("로그인 페이지")
+    void t1() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/login"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/member/login"))
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showLogin"))
+                .andExpect(content().string(containsString("""
+                        로그인
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())));
+    }
+}
