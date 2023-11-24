d45a93d9e66d0f027ae434e339648f462274180c
mabyoungg <mabyoungg@gmail.com>
2023-11-25 00:47:06 +0900

    test: 로그인 test code

--- src/test/java/com/example/likelionspringboot/domain/member/member/controller/MemberControllerTest.java
+++ src/test/java/com/example/likelionspringboot/domain/member/member/controller/MemberControllerTest.java
@@ -11,6 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -48,5 +51,31 @@ public class MemberControllerTest {
                 .andExpect(content().string(containsString("""
                         <input type="password" name="password"
                         """.stripIndent().trim())));
+    }
+
+    // POST /member/login
+    @Test
+    @DisplayName("잘못된 로그인")
+    void t2() throws Exception {
+        mvc.perform(
+                        formLogin("/member/login")
+                                .user("username", "user1")
+                                .password("password", "12345")
+                )
+                .andDo(print())
+                .andExpect(unauthenticated());
+    }
+
+    // POST /member/login
+    @Test
+    @DisplayName("로그인")
+    void t3() throws Exception {
+        mvc.perform(
+                        formLogin("/member/login")
+                                .user("username", "user1")
+                                .password("password", "1234")
+                )
+                .andDo(print())
+                .andExpect(authenticated());
     }
 }
